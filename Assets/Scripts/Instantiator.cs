8f2fdb4bebb7b02b601141816df5ac3f7ef10ff9
T-CZERWIAK <157724722+t-czerwiak@users.noreply.github.com>
2025-05-13 11:58:55 -0300

    terminado

---
+++ Assets/Scripts/Instantiator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    public GameObject cuboPrefab;
+    void Start()
+    {
+        Instantiate(cuboPrefab);
+    }
+
+    public void instanciarCubo()
+    {
+        Instantiate(cuboPrefab);
+    }
+}
